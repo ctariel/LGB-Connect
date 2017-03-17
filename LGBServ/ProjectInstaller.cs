43e4b8c6945bc4d6d8995b8dcb0708ba23f320b2
ctariel <christophe.tariel@laposte.net>
2017-03-17 16:16:43 +0100

    V0.5.1

--- LGBServ/ProjectInstaller.cs
+++ LGBServ/ProjectInstaller.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.Linq;
+using System.ServiceProcess;
 using System.Threading.Tasks;
 
 namespace LGBServ
@@ -14,11 +15,26 @@ namespace LGBServ
         public ProjectInstaller()
         {
             InitializeComponent();
+            this.Committed += new InstallEventHandler(ProjectInstaller_Committed);
         }
 
         private void serviceProcessInstaller_LGB_AfterInstall(object sender, InstallEventArgs e)
         {
 
+        }
+
+        protected override void OnCommitted(System.Collections.IDictionary savedState)
+        {
+            ServiceController sc = new ServiceController("Service_LGB");
+            sc.Start();
+        }
+        private void ProjectInstaller_Committed(object sender, InstallEventArgs e)
+        {
+            ServiceController sc = new ServiceController("Service_LGB");
+            if (sc.Status == ServiceControllerStatus.Stopped)
+            {
+                sc.Start();
+            }
         }
     }
 }
